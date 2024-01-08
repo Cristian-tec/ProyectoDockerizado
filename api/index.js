1a25bce063d05c1a53b4e080fa4ef74bc0cde208
Cristian <89481577+Cristian-tec@users.noreply.github.com>
2024-01-08 16:41:36 -0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,32 @@
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+const { createDiets } = require('./src/controllers/controllers')
+require('dotenv').config();
+const { PORT } = process.env;
+
+conn.sync({ force: true }).then(() => {
+  server.listen(process.env.PORT, () => {
+    createDiets();
+    console.log(`API Food listening at ---> ${PORT}`); // eslint-disable-line no-console
+  });
+});
+//Creado por Cristian Ganon
